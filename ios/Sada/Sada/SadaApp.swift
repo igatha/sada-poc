f852ff4d23f3d06a3a04dfedee7e323b1a818bfe
Nizar <nizarmah@hotmail.com>
2025-05-07 11:34:51 +0300

    ios: Create base app

---
+++ ios/Sada/Sada/SadaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SadaApp.swift
+//  Sada
+//
+//  Created by Nizar Mahmoud on 07/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SadaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
